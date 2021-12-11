8df313932d418469eb39ed7a6d2e4461a5f79002
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-12 00:37:24 +0300

    convert function based views to class based APIView

--- newsletter/news/api/urls.py
+++ newsletter/news/api/urls.py
@@ -5,6 +5,10 @@ from news.api import views as api_views
 # route begins with api that we created settings.py
 urlpatterns = [
   # our api will looks like /api/articles/
-  path('articles/', api_views.article_list_create_api_view, name="article-list"),
-  path('articles/<int:pk>', api_views.article_detail_api_view, name="article-detail"),
+  # class based views
+  path('articles/', api_views.ArticleListCreateView.as_view(), name="article-list"),
+  path('articles/<int:pk>', api_views.ArticleDetailAPIView.as_view(), name="article-detail"),
+  # function based views
+  # path('articles/', api_views.article_list_create_api_view, name="article-list"),
+  # path('articles/<int:pk>', api_views.article_detail_api_view, name="article-detail"),
 ]
--- newsletter/news/api/views.py
+++ newsletter/news/api/views.py
@@ -6,9 +6,74 @@ from rest_framework.decorators import api_view
 from news.models import Article
 from news.api.serializers import ArticleSerializer
 
+# Class views
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 
-# GET request beklediğimizi belirtiyoruz
-@api_view(['GET', 'POST'])
+class ArticleListCreateView(APIView):
+    # response to GET request
+    def get(self, request):
+        articles = Article.objects.filter(is_active=True)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    # response to POST request
+    def post(self, request):
+      serializer = ArticleSerializer(data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDetailAPIView(APIView):
+    # take instance of an article and return
+    def get_object(self, pk):
+      article_instance = get_object_or_404(Article, pk=pk)
+      return article_instance
+    
+    # and response to client GET method by returning captured article above
+    def get(self, request, pk):
+      # get_object is our method defined above
+      article = self.get_object(pk=pk)
+      serializer = ArticleSerializer(article)
+      return Response(serializer.data)
+    
+    # response to put request by comparing two data, one of them coming from client
+    # one of them from our db(we understand which data come from by pk parameter)
+    # if data comes as expected, then we save serializer and return updated data to client
+    # else we return 400 BAD request response
+    def put(self, request, pk):
+      article = self.get_object(pk=pk)
+      # here our Article Serializer compare our instance and coming data from client
+      serializer = ArticleSerializer(article, data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      # we also add errors coming from our serializer
+      # for example if client try to update field with wrong data structure
+      # such as pub_date : false, then client will take this response
+      # {
+      #   "pub_date": [
+      #     "Date has wrong format. Use one of these formats instead: YYYY-MM-DD."
+      #   ]
+      # } 
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # response to delete request as name suggested,
+    # run our get_object method and capture related data
+    # then delete and return 204 status
+    def delete(self, request, pk):
+      article = self.get_object(pk=pk)
+      article.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+# FUNCTION METHOD
+# GET ve POST request beklediğimizi belirtiyoruz
+"""@api_view(['GET', 'POST'])
 def article_list_create_api_view(request):
     
     if request.method == 'GET':
@@ -67,6 +132,6 @@ def article_detail_api_view(request, pk):
           }
         },
         status=status.HTTP_204_NO_CONTENT
-      )
+      )"""
 
 
