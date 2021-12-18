bb7b12e94b325c10e4d691d52e9fe7de17091d67
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-18 21:06:10 +0300

    create models of Book , Comment and register on Admin page

---
+++ newsletter/books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Book(models.Model):
+  name = models.CharField(max_length=255, verbose_name=_('Name'))
+  author = models.CharField(max_length=255, verbose_name=_('Author'))
+  description = models.CharField(max_length=255, verbose_name=_('Description'), blank=True)
+  published_at = models.DateTimeField(verbose_name=_('Published date'))
+  created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created date'))
+  updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated date'))
+
+  def __str__(self):
+    return f"{self.name}-{self.author}"
+
+
+class Comment(models.Model):
+  book = models.ForeignKey(Book, verbose_name=_('Book'), on_delete=models.CASCADE, related_name='comments')
+  owner = models.CharField(max_length=255, verbose_name=_('Owner'))
+  comment = models.TextField(blank=True, null=True, verbose_name=_('Comment'))
+  rating = models.PositiveBigIntegerField(verbose_name=_('Rating'),
+    validators=[MinValueValidator(1), MaxValueValidator(5)]
+  )
+  created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created date'))
+  updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated date'))
+
+  def __str__(self):
+    return str(self.rating)
