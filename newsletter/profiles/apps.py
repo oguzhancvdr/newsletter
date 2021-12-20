7da317e6249c4d7e49015fd8e23000614af62524
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-21 00:53:13 +0300

    create signal and import with def ready method

--- newsletter/profiles/apps.py
+++ newsletter/profiles/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+
+    # biz signal dosyamızda profiles'ı çoktan import ettik
+    # yani signals.py yüklenmeden önce bu profillermin yüklenmesi lazım
+    # bu profiller signals.py içindeki,
+    # bundan dolayı bu app dosyası okunmaya hazır olduğunda
+    # signal dosyamızı direk import etmesini istiyoruz
+
+    def ready(self):
+        import  profiles.signals
---
+++ newsletter/profiles/signals.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from profiles.models import Profile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# User modelimizde bir işlem gerçekleştiğinde;
+# yani yeni bir user oluşturulup kaydedildiğinde
+# post_save signal methodu benim aşağıda oluşturduğum create_profil functionumu
+# tetiklesin bu signali aldıktan sonra
+# burda biz sisteme bir kullanıcı dahil olduğunda otomatikmen onun
+# Profilini oluşturuyoruz
+@receiver(post_save, sender=User)
+def create_profil(sender, instance, created, **kwargs):
+    print(instance.username, '__Created: ', created)
+    if created:
+        Profile.objects.create(user=instance)
+
