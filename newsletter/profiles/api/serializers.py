d08c65c272355edfbec445ef38f9a5bc698c9c97
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-22 01:02:22 +0300

    create serialziers

---
+++ newsletter/profiles/api/serializers.py
@@ -0,0 +1,26 @@
+from django.db.models import fields
+from profiles.models import Profile, ProfileStatus
+from rest_framework import serializers
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    # burda read_only true diyoruz çünkü biz foto yüklemeyi başka serializer ile yapacağız
+    avatar = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class ProfileAvatarSerializer(serializers.ModelSerializer):
+    model = Profile
+    fields = ['avatar']
+
+
+class ProfileStatusSerializer(serializers.ModelSerializer):
+    user_profile = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = ProfileStatus
+        fields = '__all__'
