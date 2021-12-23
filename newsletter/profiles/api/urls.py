406bcfa01033d4e8caa916b06ad8b24ad580c743
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-23 22:24:10 +0300

    apply DefaultRouter of rest_framework

--- newsletter/profiles/api/urls.py
+++ newsletter/profiles/api/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from profiles.api.views import ProfileList
+from django.urls.conf import include
+from profiles.api.views import ProfileViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'user_profiles', ProfileViewSet)
+
+# before router implementation
+# profile_list = ProfileViewSet.as_view({'get': 'list'})
+# profile_detail = ProfileViewSet.as_view({'get': 'retrieve'})
 
 urlpatterns = [
-  path('user_profiles/', ProfileList.as_view(), name="profiles" )
+  # path('user_profiles/', profile_list, name="profiles" ),
+  # path('user_profiles/<int:pk>',profile_detail, name="profile_detail" ),
+  path('', include(router.urls)),
 ]
