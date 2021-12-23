cf73c02896b8335f09695a4cb1aba2035704b515
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-24 00:17:58 +0300

    create permission for avatar update only for owner

--- newsletter/profiles/api/permissions.py
+++ newsletter/profiles/api/permissions.py
@@ -7,3 +7,11 @@ class IsProfileOwnerOrReadOnly(permissions.BasePermission):
           return True
       return request.user == obj.user
 
+
+class IsStatusOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+      if request.method in permissions.SAFE_METHODS:
+          return True
+      # breakpoint()
+      return request.user.profile == obj.user_profile
+
