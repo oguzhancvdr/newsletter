334d0106e42ac2f1197c982bdcbb14ea297ec938
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-23 22:57:57 +0300

    create permissions and update viewset

--- newsletter/profiles/api/views.py
+++ newsletter/profiles/api/views.py
@@ -1,12 +1,15 @@
-from rest_framework import generics, serializers
+from rest_framework import generics, serializers, mixins
 from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from profiles.models import Profile
 from profiles.api.serializers import ProfileSerializer
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
+from profiles.api.permissions import IsProfileOwnerOrReadOnly
 
 
-class ProfileViewSet(ReadOnlyModelViewSet):
+class ProfileViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin, GenericViewSet):
+
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsProfileOwnerOrReadOnly]
