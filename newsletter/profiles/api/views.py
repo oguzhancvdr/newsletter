88e13e7acca59850b9ec47c6ae1b173131a22b13
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-23 22:23:54 +0300

    covert generic views to viewsets

--- newsletter/profiles/api/views.py
+++ newsletter/profiles/api/views.py
@@ -3,9 +3,10 @@ from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from profiles.models import Profile
 from profiles.api.serializers import ProfileSerializer
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 
-class ProfileList(generics.ListAPIView):
+class ProfileViewSet(ReadOnlyModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
